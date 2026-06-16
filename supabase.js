f7f828a831ee29528b0c94d99f3bf865639e8b8c
yyayuxsh <172826454+yyayuxsh@users.noreply.github.com>
2026-06-16 19:42:55 +0530

    Add Supabase configuration

---
+++ supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://dfsppwmuzwgkixmbqmvi.supabase.co";
+
+const SUPABASE_ANON_KEY = "sb_publishable_6ffW1Z9m7NYWrY3aS0Bh1g_ZwLtSJPD";
+
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
