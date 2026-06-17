f10450e831ede9492c6e66787bbc7931842d2c8c
yyayuxsh <172826454+yyayuxsh@users.noreply.github.com>
2026-06-17 10:15:20 +0530

    Rename lunaSupabase to window.lunaSupabase

--- supabase.js
+++ supabase.js
@@ -2,7 +2,7 @@ const SUPABASE_URL = "https://dfsppwmuzwgkixmbqmvi.supabase.co";
 
 const SUPABASE_ANON_KEY = "sb_publishable_6ffW1Z9m7NYWrY3aS0Bh1g_ZwLtSJPD";
 
-const lunaSupabase = window.supabase.createClient(
+window.lunaSupabase = window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_ANON_KEY
 );
