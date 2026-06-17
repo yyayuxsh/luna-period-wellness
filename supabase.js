176600f66d0f0b5163ff011cd085e62550b2bc45
yyayuxsh <172826454+yyayuxsh@users.noreply.github.com>
2026-06-17 09:14:35 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 const SUPABASE_URL = "https://dfsppwmuzwgkixmbqmvi.supabase.co";
 
-const SUPABASE_ANON_KEY = "YOUR_PUBLISHABLE_KEY";
+const SUPABASE_ANON_KEY = "sb_publishable_6ffW1Z9m7NYWrY3aS0Bh1g_ZwLtSJPD";
 
 const lunaSupabase = window.supabase.createClient(
   SUPABASE_URL,
